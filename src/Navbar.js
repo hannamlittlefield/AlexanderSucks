6851469d541f4faa55b42b2d5d25d2a5957e85ef
HANNA_LITTLEFIELD <hannamlittlefield@gmail.com>
2021-11-18 10:55:22 -0500

    adding routes and switch routes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import Projects from './Projects'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default function Navbar(){
     return (
+      <Router>
         <div>
               <ul id="nav">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/">Contact</Link></li>
               </ul>
+
+              <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projets/>
+          </Route>
+        </Switch>
             </div>
+      </Router>
+
       );
     };
