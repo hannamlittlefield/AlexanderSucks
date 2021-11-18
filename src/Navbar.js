136893e4f0484a433861ffc76d56bad7e1f0418b
Hanna <hannamlittlefield@gmail.com>
2021-11-18 07:36:35 -0500

    adding links to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function Navbar(){
     return (
-
+        <div>
+              <ul id="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
       );
     };
