b9ac0a4ec7a9a9b3deb360ff4028f0f32b9cb1ce
Hanna <hannamlittlefield@gmail.com>
2021-12-14 16:13:35 -0500

    adding padding to link css

--- src/Projects.js
+++ src/Projects.js
@@ -10,8 +10,6 @@ export default function Projects(){
         <img className="projImg" src="https://i.postimg.cc/c45wtbww/BQProject.jpg"/><br/>
                 <a href="https://codepen.io/hannamlittlefield/pen/MWJLPgx" target="_blank">CodePen</a> 
                 <a href="https://github.com/hannamlittlefield/BQSceneSelector/tree/master/BQSceneGen/bq-app/src" target="_blank">Github</a>
-            <div class="hexagon"/>
-            <div class="hexagon"/>
         </div>
     )
 }
