d1144ff46d445177fa76ed35e070c27e3e1364b7
Hanna <hannamlittlefield@gmail.com>
2022-01-04 11:16:23 -0500

    changing index.js to follow along w react router tutortial

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,13 +7,7 @@ export default function NavTopbar(){
       <div style={{ display: 'block', width: 700, padding: 30 }}>
       <Navbar bg="#ede7f6">
         <Navbar.Brand href="#home">
-          <img
-src="https://media.geeksforgeeks.org/wp-content/uploads/20210425000233/test-300x297.png"
-            alt="Sample Brand Logo"
-            width="35"
-            className="align-top d-inline-block"
-            height="35"
-          />Test Company
+          Home
         </Navbar.Brand>
       </Navbar>
     </div>
