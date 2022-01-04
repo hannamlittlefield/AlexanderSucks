16ab78de11dde4c8ae6de80361f7efacf17a7acf
Hanna <hannamlittlefield@gmail.com>
2022-01-04 11:29:14 -0500

    adding Links in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 
 export default function NavTopbar(){
     return (
       <div style={{ display: 'block', width: 700, padding: 30 }}>
       <Navbar bg="#ede7f6">
-        <Navbar.Brand href="#home">
-          Home
-        </Navbar.Brand>
+        <Link to="/home">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
       </Navbar>
     </div>
   );
