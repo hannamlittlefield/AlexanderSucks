13807952a80f22e2b92b8acb6aa0368786fd2330
Hanna <hannamlittlefield@gmail.com>
2022-01-04 16:02:53 -0500

    adding in basic css background to pages

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About(){
     return(
-        <div align="center" className="Projects">
+        <div align="center" className="About">
             {/*<p>Hi! I'm Hanna - a self taught programmer enjoying life in Massachusetts. After graduating with a business management degree and spending eight years in the field, I left the business world behind and began to pursue my passion for coding.
             
             Through various online resources and routine work with a mentor - I have taught myself a variety of languages, libraries, and web development skills. I always enjoy a challenge, and have the ability to teach myself nearly anything that I put my mind to. 
--- src/Contact.js
+++ src/Contact.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Contact(){
     return(
-        <a href = "mailto: HannaMLittlefield@gmail.com">Email</a>                        
+        <div align="center" className="Contact">
+        <a href = "mailto: HannaMLittlefield@gmail.com">Email</a>           
+        </div>             
     )
 }
