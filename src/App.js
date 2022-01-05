3265d1f522e67c17a4d447914efed58d839937a7
Hanna <hannamlittlefield@gmail.com>
2022-01-05 17:11:35 -0500

    adding body bg color

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import {Outlet} from 'react-router-dom'
 import './App.css';
 import NavTopBar from './Navbar';
-import Projects from './Projects';
+import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
 
+const StyledCont = styled(Container)`
+background-color: #e2e2e2;
+height: 100vh;
+`
 
 export default function App() {
   return (
