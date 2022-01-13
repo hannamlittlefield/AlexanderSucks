181c6cd104e9aed804d477d0feda142c7971245a
Hanna <hannamlittlefield@gmail.com>
2022-01-13 14:54:34 -0500

    adding norefferer in links

--- src/Projects.js
+++ src/Projects.js
@@ -6,9 +6,13 @@ import { FaGithub, FaCodepen } from "react-icons/fa";
 export default function Projects(){
     return(
         <div align="center" className="Projects">
-        <img className="projImg" src={projectpic}/><br/>
-                <FaCodepen/><a href="https://codepen.io/hannamlittlefield/pen/MWJLPgx" target="_blank"><FaGithub/></a>
-                <FaGithub/><a href="https://github.com/hannamlittlefield/BQSceneSelector/tree/master/BQSceneGen/bq-app/src" target="_blank">Github</a>
+        <img className="projImg" src={projectpic} alt="A screen capture of my scene generator project"/><br/>
+                <FaCodepen/><a href="https://codepen.io/hannamlittlefield/pen/MWJLPgx" target="_blank" rel="noreferrer"><FaGithub/></a>
+                <FaGithub/><a href="https://github.com/hannamlittlefield/BQSceneSelector/tree/master/BQSceneGen/bq-app/src" target="_blank" rel="noreferrer">Github</a>
         </div>
     )
 }
+
+//verify that projects component rendered
+//verify that there is X amount of images
+//verify that there are codepen and github icons and has an anchor tag to appropriate link
