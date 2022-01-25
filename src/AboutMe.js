c029273f720f5174163a6578e4204d9906c95175
Hanna <hannamlittlefield@gmail.com>
2022-01-25 08:10:59 -0500

    Adding description to about me page and fluid container

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -2,16 +2,15 @@ import React from 'react'
 
 export default function About(){
     return(
-        <div align="center" className="About">
-            {/*<p>Hi! I'm Hanna - a self taught programmer enjoying life in Massachusetts. After graduating with a business management degree and spending eight years in the field, I left the business world behind and began to pursue my passion for coding.
+        <div align="center" className="About" class="container-fluid">
+            <p>Hi! I'm Hanna - a self taught programmer enjoying life in Massachusetts. After graduating with a business management degree and spending eight years in the field, I left the business world behind and began to pursue my passion for coding.<br/>
             
-            Through various online resources and routine work with a mentor - I have taught myself a variety of languages, libraries, and web development skills. I always enjoy a challenge, and have the ability to teach myself nearly anything that I put my mind to. 
+            Through various online resources and routine work with a mentor - I have taught myself a variety of languages, libraries, and web development skills. I always enjoy a challenge, and have the ability to teach myself nearly anything that I put my mind to.<br/> 
 
-            I am passionate about my fur-family, to-do lists, running obscenely long distances, creating things with my hands, and solving puzzles. 
-    </p>*/}
-    <h1>La la la short write about about meee. </h1>
-    <h2> maybe make a d3 timeline of what I've been learning</h2>
-    <h3>Probably format a nice resume here too</h3>
+            I am passionate about my fur-family, reading a lot of b ooks, to-do lists, running obscenely long distances, creating things with my hands, and solving puzzles. 
+    </p>
+
+    <p>You can learn more about my skills by checking out my resume!</p>
         </div>
     )
 }
