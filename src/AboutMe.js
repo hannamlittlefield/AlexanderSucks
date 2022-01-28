28032dbaa9d4f55f629f5dbe9f6a49089244f763
Hanna <hannamlittlefield@gmail.com>
2022-01-28 08:14:59 -0500

    adding col/row to about me page

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,13 +7,13 @@ import Container from 'react-bootstrap/Container'
 
 export default function About(){
     return(
-        <Container fluid="md">
+        <Container>
             <Row>
-                <Col xs>
+                <Col>
                     <img className="projImg" src={ProfPic} alt="A photo of Hanna looking off to the side laughing"/>
                 </Col>
                 <Col>
-                    <div align="center" className="About"> 
+                    <div align="center"> 
                         <p>Hi! I'm Hanna - a self taught programmer enjoying life in Massachusetts. After graduating with a business management degree and spending eight years in the field, I left the business world behind and began to pursue my passion for coding.</p>
                         
                         <p>Through various online resources and routine work with a mentor - I have taught myself a variety of languages, libraries, and web development skills. I always enjoy a challenge, and have the ability to teach myself nearly anything that I put my mind to.</p> 
@@ -24,8 +24,7 @@ export default function About(){
                     </div>
                 </Col>
             </Row>
-            <Row>
-  </Row>
         </Container>
+        
     )
 }
