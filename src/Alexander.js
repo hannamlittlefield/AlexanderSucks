26a32f029846b38bf64114cdb19d29df4b39fad8
Hanna <hannamlittlefield@gmail.com>
2023-02-03 21:46:32 -0500

    routing

--- src/Alexander.js
+++ src/Alexander.js
@@ -7,9 +7,9 @@ export default function Alex(){
         <div align="center" className="alexandersucks">
         <div id="projparagraph">
             Ever have the urge to take useless online quizzes as a way to burn time or money? We got you covered (: Check out these completely valid, very accurate quizzes! </div>
-        <ul><a href="/dessertquiz">What dessert are you?</a></ul>
-        <ul><a href="/vacationquiz">Answer a few questions and we'll tell you where you should go on your next trip!</a></ul>
-        <ul><a href="/zodiacquiz">Can I guess your zodaic sign?</a>
+        <ul><a href="AlexanderSucks/dessertquiz">What dessert are you?</a></ul>
+        <ul><a href="AlexanderSucks/vacationquiz">Answer a few questions and we'll tell you where you should go on your next trip!</a></ul>
+        <ul><a href="AlexanderSucks/zodiacquiz">Can I guess your zodaic sign?</a>
         </ul>
         <p className="lovenote">I hope you enjoy these, laugh a little bit, and have fun. Happy discount chocolate day, babes. I adore you.</p>
         <p className="lovehan anim-typewriter">Love, hanna</p>
