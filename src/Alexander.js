1e15f62ddff3a7e6760712df3da9644b5cbda8ad
Hanna <hannamlittlefield@gmail.com>
2023-02-03 23:21:58 -0500

    edits

--- src/Alexander.js
+++ src/Alexander.js
@@ -6,7 +6,7 @@ export default function Alex(){
     return(
         <div align="center" className="alexandersucks">
         <div id="projparagraph">
-            Ever have the urge to take useless online quizzes as a way to burn time or money? We got you covered (: Check out these completely valid, very accurate quizzes! </div>
+            Ever have the urge to take useless online quizzes as a way to waste some time? I gotchu, babes. Check out these completely valid, very accurate quizzes! </div>
         <ul><a href="/dessertquiz">What dessert are you?</a></ul>
         <ul><a href="/vacationquiz">Answer a few questions and we'll tell you where you should go on your next trip!</a></ul>
         <ul><a href="/zodiacquiz">Can I guess your zodaic sign?</a>
