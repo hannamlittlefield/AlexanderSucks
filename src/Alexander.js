1c9d6c11d7446a522ae18268fad935c1c9c10f39
Hanna <hannamlittlefield@gmail.com>
2023-02-05 17:34:37 -0500

    routing

--- src/Alexander.js
+++ src/Alexander.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './App.css';
 
 
@@ -7,7 +8,8 @@ export default function Alex(){
         <div align="center" className="alexandersucks">
         <div id="projparagraph">
             Ever have the urge to take useless online quizzes as a way to waste some time? I gotchu, babes. Check out these completely valid, very accurate quizzes that I definitely did not make up. </div>
-        <ul><a href="/dessertquiz">What dessert are you?</a></ul>
+        <ul><Link to="/dessertquiz">What dessert are you?</Link>
+        <a href="/dessertquiz">What dessert are you?</a></ul>
         <ul><a href="/vacationquiz">Answer a few questions and we'll tell you where you should go on your next trip!</a></ul>
         <ul><a href="/zodiacquiz">Can I guess your zodiac sign?</a></ul>
         <ul><a href="/meow">Which cat are you?</a>
