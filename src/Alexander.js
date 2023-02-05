4113007e2738c8dfda4f193e55ae3169ecfd0fcd
Hanna <hannamlittlefield@gmail.com>
2023-02-05 00:23:03 -0500

    adding a link

--- src/Alexander.js
+++ src/Alexander.js
@@ -13,7 +13,8 @@ export default function Alex(){
         <ul><a href="/meow">Which cat are you?</a>
         <ul><a href="/ididit">How well do you know your wildlife?</a></ul>
         <ul><a href="/areweserious">If you HAD to be a Buffy character, who would you be?</a></ul>
-        <ul><a href="/eggggiiies"> Which eggie dish are you?</a></ul>
+        <ul><a href="/eggggiiies">Which eggie dish are you?</a></ul>
+        <ul><a href="/eggggiiies">I bet I can guess your dance stlye in a few short questions!</a></ul>
         </ul>
         <p className="lovenote">Dear Alexander, I hope you enjoy these, laugh a little bit, and have fun. They were really enjoyable to make, and I had a great time coding for fun and not for clients or work.Happy discount chocolate day, babes. I adore you, I love you, and you are my favorite.</p>
         <p>Love,</p>
